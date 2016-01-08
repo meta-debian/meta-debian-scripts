3eea55c6fa56cc99490749abc4c103b77ae4504e
Masahiro Yamada <masahiro17.yamada@toshiba.co.jp>
2016-01-08 14:25:21 +0900

    Add a script which generates all source list automatically.
    Signed-off-by: Masahiro Yamada <masahiro17.yamada@toshiba.co.jp>

---
+++ repo-lists/generate_src-jessie_meta-debian_all.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+USER=ystk
+OUTPUTFILE="src-jessie_meta-debian_all.txt"
+
+function abort
+{
+	echo "ERROR: $@" 1>&2
+	exit 1
+}
+
+# Get the last page number.
+LASTPAGE=`curl -I "https://api.github.com/users/"$USER"/repos?per_page=100" 2> /dev/null \
+			| grep "rel=\"last\"" | cut -d";" -f2 | cut -d"=" -f4 | cut -d">" -f1`
+[ -z $LASTPAGE ] && abort "Cannot get the last page number from github."
+LASTPAGE=`expr $LASTPAGE + 1`
+
+if [ -f $OUTPUTFILE ]; then
+	rm $OUTPUTFILE
+fi
+
+# Get all name of "debian-*" repository and write output files.
+for ((i=1; i<$LASTPAGE; i++));
+do
+	curl "https://api.github.com/users/"$USER"/repos?page="$i"&per_page=100" 2> /dev/null \
+		| jq '[.[] .name]' | grep "\"debian-" | cut -d"\"" -f2 >> $OUTPUTFILE;
+done
+sed -e 's/^/ystk\//g' -i $OUTPUTFILE
+echo "meta-debian/linux-ltsi" >> $OUTPUTFILE
