feb1daf5ff68346533789cfe13d9d13e714db1ac
KOBAYASHI Yoshitake <yoshitake.kobayashi@gmail.com>
2017-03-26 09:14:20 +0900

    repo-list: Added minimal list and rename to deby
    Signed-off-by: KOBAYASHI Yoshitake <yoshitake.kobayashi@gmail.com>

---
+++ repo-lists/generate_src-deby_all.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+USER=ystk
+OUTPUTFILE="src-jessie_deby_all.txt"
+
+function abort
+{
+	echo "ERROR: $@" 1>&2
+	exit 1
+}
+
+# Get the last page number.
+LASTPAGE=`curl -I "https://api.github.com/users/"$USER"/repos?per_page=100" 2> /dev/null \
+			| grep "rel=\"last\"" | cut -d";" -f2 | cut -d"=" -f4 | cut -d">" -f1`
+[ -z $LASTPAGE ] && abort "Cannot get the last page number from github."
+LASTPAGE=`expr $LASTPAGE + 1`
+
+if [ -f $OUTPUTFILE ]; then
+	rm $OUTPUTFILE
+fi
+
+# Get all name of "debian-*" repository and write output files.
+for ((i=1; i<$LASTPAGE; i++));
+do
+	curl "https://api.github.com/users/"$USER"/repos?page="$i"&per_page=100" 2> /dev/null \
+		| jq '[.[] .name]' | grep -e "\"debian-" -e "\"gnu-config" -e "\"pseudo" \
+		-e "\"qemu" -e "\"linux-ltsi" -e "\"linux-cip" | cut -d"\"" -f2 >> $OUTPUTFILE;
+#	curl "https://api.github.com/users/"$USER"/repos?page="$i"&per_page=100" 2> /dev/null \
+#		| jq '[.[] .name]' | grep "\"gnu-config" | cut -d"\"" -f2 >> $OUTPUTFILE;
+#	curl "https://api.github.com/users/"$USER"/repos?page="$i"&per_page=100" 2> /dev/null \
+#		| jq '[.[] .name]' | grep "\"pseudo" | cut -d"\"" -f2 >> $OUTPUTFILE;
+#	curl "https://api.github.com/users/"$USER"/repos?page="$i"&per_page=100" 2> /dev/null \
+#		| jq '[.[] .name]' | grep "\"qemu" | cut -d"\"" -f2 >> $OUTPUTFILE;
+#	curl "https://api.github.com/users/"$USER"/repos?page="$i"&per_page=100" 2> /dev/null \
+#		| jq '[.[] .name]' | grep "\"linux-ltsi" | cut -d"\"" -f2 >> $OUTPUTFILE;
+done
+sed -e 's/^/ystk\//g' -i $OUTPUTFILE
